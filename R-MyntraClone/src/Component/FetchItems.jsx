a707e5d0fb1e90e9a35a3e2563f26c54bbfd430a
ALI RAZA <razadeveloper816@gmail.com>
2025-12-31 15:57:50 -0800

    remove env file

--- R-MyntraClone/src/Component/FetchItems.jsx
+++ R-MyntraClone/src/Component/FetchItems.jsx
@@ -13,7 +13,7 @@ export default function FetchItems() {
         const controller = new AbortController();
         const signal = controller.signal;
         dispatch(fetchSliceActions.markfetchingStart());
-        fetch(`${process.env.REACT_API_URL}/items`, { signal })
+        fetch('http://localhost:8080/items', { signal })
         .then((res) => res.json())
         .then(({ items }) => {
             dispatch(fetchSliceActions.markfetchDone());
